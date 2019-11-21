f295a93e1da389d1202b25c926db776edce2baf7
AlinaB5 <57999214+AlinaB5@users.noreply.github.com>
2019-11-21 19:12:18 +0200

    1.2 cleaner code

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 'use strict'
 // mineSweeper game 
 // my globals
-var MINE = '💣';
-var FLAG = '🏁';
-
+var MINE = '💣',
+    FLAG = '🏁';
 
 var gLevel = {
     size: 4,
-    mines: 2,
+    mines: 3,
     hints: 3,
 };
 
@@ -17,14 +16,14 @@ var gGame = {
     markedCount: 0,
     secsPassed: 0,
     hintMode: false,
-    lives: 3
+    lives: 0
 }
 
-var gBoard; //Matrix contains cell objects  
-var gNumOfminesToReveal;
-var gClockInterval = null;
-var gStartTime;
-var gUsedHintsCount = 0;
+var gBoard,
+    gNumOfminesToReveal,
+    gClockInterval = null,
+    gStartTime,
+    gUsedHintsCount = 0;
 
 
 function initGame(size = gLevel.size, mines = gLevel.mines, hints = gLevel.hints) {
@@ -47,50 +46,28 @@ function initGame(size = gLevel.size, mines = gLevel.mines, hints = gLevel.hints
     createHints();
 }
 
-function buildBoard() { // TODO putting it in utils.
-    gBoard = [];
-    for (var i = 0; i < gLevel.size; i++) {
-        gBoard.push([])
-        for (var j = 0; j < gLevel.size; j++) {
-            gBoard[i][j] = createCell();
-        }
-    }
-    return gBoard;
-}
-
-function createCell() {  // TODO move to utils maybe 
-    var cellObj = {
-        isShown: false,
-        isMine: false,
-        isMarked: false,
-        tempShown: false
-    }
-    return cellObj;
-}
-
-function plantMines(firstCellI, firstCellJ) {
+function plantMines(firstClickedI, firstClickedJ) {
     for (var i = 0; i < gLevel.mines; i++) {
         var mineLocation = getRandomLocation();
-        //if it wasn't a mine before put it there and is not the first clicked 
-        if (!gBoard[mineLocation.i][mineLocation.j].isMine && gBoard[mineLocation.i][mineLocation.j] !== gBoard[firstCellI][firstCellJ]) {
-            gBoard[mineLocation.i][mineLocation.j].isMine = true
+        //if it wasn't a mine before and if its not the first clicked put it there 
+        if (!gBoard[mineLocation.i][mineLocation.j].isMine && gBoard[mineLocation.i][mineLocation.j] !== gBoard[firstClickedI][firstClickedJ]) {
+            gBoard[mineLocation.i][mineLocation.j].isMine = true;
         } else {
             i--;
         }
     }
 }
 
-function setMinesNegsCount() {// Count mines around each cell and set the cell's minesAroundCount //TODO move to utils maybe 
+function setMinesNegsCount() {
     for (var i = 0; i < gBoard.length; i++) {
         for (var j = 0; j < gBoard[i].length; j++) {
             var cell = gBoard[i][j]
-            cell.minesAroundCount = countMinesAround(i, j);
+            cell.minesAroundCount = getMinesCount(i, j);
         }
     }
 }
 
-// cell.minesAroundCount = setMinesNegsCount()
-function countMinesAround(cellI, cellJ) {
+function getMinesCount(cellI, cellJ) {
     var minesCount = 0;
     for (var i = cellI - 1; i <= cellI + 1; i++) {
         if (i < 0 || i >= gBoard.length) continue;
@@ -173,7 +150,7 @@ function cellClicked(event, cellI, cellJ) {
             }
         }
 
-        if (cell.minesAroundCount === 0 || gGame.hintMode) expandShown(cellI, cellJ);
+        if ((cell.minesAroundCount === 0 && !cell.isMine) || gGame.hintMode) expandShown(cellI, cellJ);
 
         checkGameOver();
         renderBoard();
@@ -181,8 +158,6 @@ function cellClicked(event, cellI, cellJ) {
 }
 
 function expandShown(cellI, cellJ) {
-    // When user clicks a cell with no mines around, we need to open not only that cell, but also its neighbors.
-    // NOTE: start with a basic implementation that only opens the non-mine 1st degree neighbors
     for (var i = cellI - 1; i <= cellI + 1; i++) {
         if (i < 0 || i >= gBoard.length) continue;
 
@@ -191,17 +166,15 @@ function expandShown(cellI, cellJ) {
             if (j < 0 || j >= gBoard[0].length) continue;
 
             if (i === cellI && j === cellJ) continue;
-            if (!gBoard[i][j].isMine && !gGame.hintMode) gBoard[i][j].isShown = true//cellClicked(i, j);
-
-            if (gGame.hintMode) {
+            if (!gGame.hintMode) {
+                if (!gBoard[i][j].isMine) gBoard[i][j].isShown = true
+            } else {
                 gBoard[i][j].tempShown = true;
                 hideRevealedByHint(cellI, cellJ)
             }
         }
     }
 }
-
-
 
 function cellMarked(cellI, cellJ) {
     if (gBoard[cellI][cellJ].isMarked) {
@@ -234,23 +207,20 @@ function checkGameOver() {
     }
 }
 
-function gameOver(won) {
+function gameOver(isWon) {
     if (gGame.hintMode) return
-    console.log('gameOver')
     gGame.isOn = false;
     var elRestartButton = document.querySelector('.restart');
-    if (won) {
+    if (isWon) {
+
         var audioWon = new Audio('/sounds/Cheering.mp3');
         audioWon.play();
-        
-        elRestartButton.innerHTML = `<img class="smileyButton"
-    src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/232/smiling-face-with-sunglasses_1f60e.png" />`;
+        elRestartButton.innerHTML = `<img class="smileyButton" src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/232/smiling-face-with-sunglasses_1f60e.png" />`;
     } else {
         revealMines();
         var audioLost = new Audio('/sounds/Bomb.mp3');
         audioLost.play();
-        elRestartButton.innerHTML = `<img class="smileyButton"
-    src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/232/dizzy-face_1f635.png" />`;
+        elRestartButton.innerHTML = `<img class="smileyButton" src="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/232/dizzy-face_1f635.png" />`;
     }
     clearInterval(gClockInterval);
 }
@@ -276,7 +246,7 @@ function activateHintMode(elHint) {
 }
 
 function hideRevealedByHint(cellI, cellJ) {
-    setTimeout(function () { // hide what hint revealed
+    setTimeout(function () {
         for (var i = cellI - 1; i <= cellI + 1; i++) {
             if (i < 0 || i >= gBoard.length) continue;
             for (var j = cellJ - 1; j <= cellJ + 1; j++) {
@@ -289,24 +259,4 @@ function hideRevealedByHint(cellI, cellJ) {
         gGame.hintMode = false;
         renderBoard()
     }, 1000);
-}
-
-function createHints() {
-    var elHintsDiv = document.querySelector('.hints');
-    elHintsDiv.innerText = '';
-    for (var i = 1; i <= gLevel.hints; i++) {
-        elHintsDiv.innerHTML += `<span class='hint${i}' onclick='activateHintMode(this)'>💡</span>`;
-    }
-}
-
-function createLives() {
-    var elLivesDiv = document.querySelector('.lives');
-    elLivesDiv.innerText = '';
-    // if (gGame.lives === 1) elLivesDiv.innerText = '0 left';
-    // else {
-    for (var i = 0; i < gGame.lives; i++) {
-        elLivesDiv.innerHTML += `<span>❤️</span>`;
-    }
-    // }
-    renderBoard();
 }
--- js/utils.js
+++ js/utils.js
@@ -1,55 +1,64 @@
 'use strict'
-
-//create board function 
-function createBoard(num) {
-  var board = [];
-  for (var i = 0; i < num; i++) {
-    board.push([]);
-    for (var j = 0; j < num; j++) {
-      board[i][j] = '';
-    }
-  }
-  return board;
-}
-
-//countNeighbors
-
-function countMinesAround(pos) {
-  var neighborsCount = 0;
-  for (var i = pos.i - 1; i <= pos.i + 1; i++) {
-    // if i is out of boundaries - go to the next i
-    if (i < 0 || i >= gBoard.length) continue; //continue to the next i
-
-    for (var j = pos.j - 1; j <= pos.j + 1; j++) {
-      // if j is out of boundaries - go to the next j:
-      if (j < 0 || j >= gBoard[0].length) continue; // continue to the next j.
-
-      if (i === pos.i && j === pos.j) continue;
-      if (gBoard[i][j] === 'X') neighborsCount++;
-    }
-  }
-  return neighborsCount;
+function setLevel(elLevelButton) {
+  var level = elLevelButton.innerText
+  if (level === 'Beginner') initGame(4, 3, 3);
+  if (level === 'Medium') initGame(8, 12, 2);
+  if (level === 'Expert') initGame(12, 30, 1);
 }
 
 
-// generate random number 
+function buildBoard() {
+  gBoard = [];
+  for (var i = 0; i < gLevel.size; i++) {
+    gBoard.push([])
+    for (var j = 0; j < gLevel.size; j++) {
+      gBoard[i][j] = createCell();
+    }
+  }
+  return gBoard;
+}
+
+function createCell() {
+  var cellObj = {
+    isShown: false,
+    isMine: false,
+    isMarked: false,
+    tempShown: false
+  }
+  return cellObj;
+}
+
+function createHints() {
+  var elHintsDiv = document.querySelector('.hints');
+  elHintsDiv.innerText = '';
+  for (var i = 1; i <= gLevel.hints; i++) {
+    elHintsDiv.innerHTML += `<span class='hint${i}' onclick='activateHintMode(this)'>💡</span>`;
+  }
+}
+
+function createLives() {
+  var elLivesDiv = document.querySelector('.lives');
+  elLivesDiv.innerText = '';
+  for (var i = 0; i < gGame.lives; i++) {
+    elLivesDiv.innerHTML += `<span>❤️</span>`;
+  }
+  renderBoard();
+}
+
 function getRandomNumber(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-// get random location from board
 function getRandomLocation() {
   var location = {
-      i: getRandomNumber(0, gLevel.size - 1),
-      j: getRandomNumber(0, gLevel.size - 1)
+    i: getRandomNumber(0, gLevel.size - 1),
+    j: getRandomNumber(0, gLevel.size - 1)
   };
   return location
 }
 
-
 function clock() {
   var timePassed = Date.now() - gStartTime;
-  // timePassed = Math.floor(timePassed/1000)
   gGame.secsPassed = Math.floor(timePassed / 1000)
   var elClock = document.querySelector('.clock');
   elClock.innerText = gGame.secsPassed
